73153fa558ec8a53fdaa4648b36f760f70f869d7
Isabella Ianora <iianora@nd.edu>
2019-06-04 18:00:52 -0400

    Classwork

---
+++ Day03/handout3answers.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun  4 16:45:18 2019
+
+@author: isabellaianora
+"""
+
+"""
+1)While loop uses a conditional statement and a variable and a for loop uses a variable and is usually over a range of numbers.
+
+2)It took one iteration to get pi accurate to at least 6 decimal places; it took two iterations to accurately calculate pi to 12 decimal places and it took three iterations to get pi completely accurate. After two iterations the error was on the magnitude of 10**-16.
+ 
+3)The two ways to open a file are using f=open(filename) and with open(filename) this method is better because you don't have to close it after like you do with the first method. Chosing a method depends on the size of your file. 
+
+4)With statements are safer to use because you don't have to close out the file after you open it. 
+
+5)An iterable is whatever you are iterating over, it can be a list, tuple, string, or generator. 
