bb8d6a053bcf25ea69340935ceba75c82a5baeda
Isabella Ianora <iianora@nd.edu>
2019-06-04 18:03:21 -0400

    pi file

--- Day03/pi.py
+++ Day03/pi.py
@@ -3,10 +3,21 @@
 """
 import math
 
+
 def srinivasa(k):
-    """
-    """
+    
+    i=0
+    calc=0
+    coff=2*math.sqrt(2)/9801
+    while i<k:
+        calc+=(math.factorial(4*i)*(1103+26390*i))/(((math.factorial(i))**4)*396**(4*i))
+        i+=1
+        print(i)
+        
+        calc_pi=1/(coff*calc)   
+    return calc_pi, math.pi-calc_pi
+    
 
 if __name__ == '__main__':
-    iterations = 1
+    iterations = 3
     print(srinivasa(iterations))
