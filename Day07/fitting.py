683e46e483af067d82fee49098eaa34e2ed43794
Isabella Ianora <iianora@nd.edu>
2019-06-18 17:41:30 -0400

    Day 7 file

--- Day07/fitting.py
+++ Day07/fitting.py
@@ -16,6 +16,8 @@ seymour.16@nd.edu
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.optimize import curve_fit
+import statistics as stat
+
 
 
 def gaus(x, A, mu, sigma):
@@ -50,9 +52,66 @@ def fitter(x, A0, mu0, sigma0, A1, mu1, sigma1):
     """
     Function to fit to the data. Two Gaussians that... 
     """
-    return
+    
+    global G1
+    global G2
+    G1= A0 * np.exp(-(x - mu0)**2/(2 * sigma0**2)) 
+    G2= A1 * np.exp(-(x - mu1)**2/(2 * sigma1**2))
+    G_tot= G1+G2
+    return G_tot
+
 
 
 bins, counts = np.loadtxt('two_peaks.txt', unpack=True)
+x= np.linspace(0, 30, 100)
+A= np.max(counts)
+mu= stat.mean(x)
+sigma= stat.stdev(x)
 
- = curve_fit() # fitting method
+
+
+paras, _ = curve_fit(fitter, bins, counts)
+
+
+
+plt.scatter(bins, counts)
+plt.plot(x, fitter(x, *paras))
+plt.plot(x, G1)
+plt.plot(x, G2)
+
+
+plt.show()
+
+
+
+#pars, _ = curve_fit(gaus, bins, counts)
+#
+#plt.scatter(bins, counts)
+#plt.plot(x, gaus(bins, *pars))
+#plt.show()  
+
+
+#Z = curve_fit() # fitting method
+ 
+ 
+ 
+ 
+ 
+
+
+
+#import matplotlib.pyplot as plt
+#import numpy
+#from scipy.optimize import curve_fit
+#
+#x = numpy.linspace(0, 10)
+#y = 2 * x + 2 *numpy.random.rand(50) - 1  # just to get scatter
+#
+#def lin(x, m, b):
+#    return m * x + b
+#
+#pars, _ = curve_fit(lin, x, y, p0=[1., 0.])
+#
+#plt.scatter(x, y)
+#plt.plot(x, lin(x, *pars))
+#plt.show()
